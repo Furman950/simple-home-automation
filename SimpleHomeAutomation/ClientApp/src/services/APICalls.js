5b26b3bde5b2db1fc00394d0aafa74a3f29bbec1
Fermin Sandoval <ferminsandoval950@hotmail.com>
2019-10-23 22:34:32 -0600

    Added some methods for api calls

--- SimpleHomeAutomation/ClientApp/src/services/APICalls.js
+++ SimpleHomeAutomation/ClientApp/src/services/APICalls.js
@@ -1,22 +1,51 @@
 
 let apiHost = window.location.host + "/api"
 
-function publishMessage(topic, message) {
-    return postRequest(`http://${apiHost}/mqtt/publish`, {topic, message})
-}
-
-function postRequest(endpoint, obj) {
-    console.log(endpoint);
-    return fetch(endpoint, {
+function postRequest(endpoint, body) {
+    return fetch(`http://${apiHost}${endpoint}`, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(obj)
+        body: body
     })
 }
 
+function getRequest(endpoint) {
+    return fetch(`http://${apiHost}${endpoint}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    })
+}
+
+function publishMessage(topic, message) {
+    return postRequest("/mqtt/publish",
+        JSON.stringify({ topic, message }));
+}
+
+function saveUI(ui) {
+    let json = JSON.stringify(ui, (key, value) => {
+        if (typeof value === 'function') {
+            return value.prototype.constructor.name
+        }
+        else {
+            return value;
+        }
+    })
+
+    return postRequest("/ui/save", json);
+}
+
+function getUI() {
+    return getRequest("/ui/get")
+}
+
 export {
-    publishMessage
+    publishMessage,
+    saveUI,
+    getUI
 }
